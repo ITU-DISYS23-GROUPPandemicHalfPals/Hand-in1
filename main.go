55f2fd0643b6da5dbce01c3a215bfc91ee945dd5
Patrick Handberg Jessen <patj@itu.dk>
2023-09-06 12:12:52 +0200

    Fork exhange

--- main.go
+++ main.go
@@ -4,6 +4,58 @@ import (
 	"fmt"
 )
 
+var ch1 = make(chan int)
+var ch2 = make(chan int)
+
+func philosopher(seat int) {
+	count := 0
+	forks := 0
+
+	for {
+		fork := <-ch1
+
+		if fork == seat || fork == (seat+1)%5 {
+			forks++
+		} else {
+			ch2 <- fork
+		}
+
+		if forks == 2 {
+			count++
+			fmt.Println("Philosopher", seat, "has eaten", count, "portion.")
+			ch2 <- seat
+			ch2 <- (seat + 1) % 5
+		}
+	}
+}
+
+func fork(seat int) {
+	inUse := false
+
+	for {
+		if !inUse {
+			ch1 <- seat
+			inUse = true
+		} else {
+			x := <-ch2
+
+			if x != seat {
+				ch2 <- x
+			} else {
+				inUse = false
+			}
+		}
+	}
+
+}
+
 func main() {
-	fmt.Print("hello")
+	for i := 0; i < 5; i++ {
+		go fork(i)
+		go philosopher(i)
+	}
+
+	for {
+
+	}
 }
